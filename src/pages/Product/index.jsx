1a37e48ad3c8f6a022c3fcd222d6f0212f58ea3d
Usuário Convidado <Guest@PA06MICRO05.local>
2019-05-13 22:53:53 -0300

    melhorando a app

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,15 +1,81 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+
+import axios from 'axios'
 
 class Product extends Component {
     constructor(props){
         super(props);
 
-        console.log(props.match.params.id);
+        this.state = {
+            id: props.match.params.id,
+            loading: true,
+            data: {}
+        };
     }
 
+    componentDidMount(){
+        axios.all([
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}`),
+            axios.get(`https://api.mercadolibre.com/items/${this.state.id}/description`)    
+        ]).then(([item, description]) => {
+                console.log(item, description)
+                this.setState({
+                    data: {
+                        ...item.data,
+                        description: description.data.plain_text
+                    },
+                    loading: false
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+
     render(){
+        //console.log(this.state.data)
+        const content = this.state.loading ? <div>Carregando</div> : <div>oi</div>
+
+        const { data } = this.state
+        if (!Object.keys(data).length){
+            return (
+                <Fragment>
+                    { content }
+                </Fragment>
+                
+            );
+        }
+
         return (
-            <div>Product</div>
+            <Fragment>
+                <div className="mdl-shadow--2dp">
+
+                    <div className="mdl-grid">
+                        <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet">
+                            <img src={ data.pictures[0].url } />
+                        </div>
+
+                        <div class="mdl-cell mdl-cell--6-col mdl-cell--8-col-tablet">
+                            <h1>{ data.title }</h1>
+
+                            <button type="button" className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+                                Comprar
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="mdl-grid">
+                        <div className="mdl-cell">
+                            <p> { data.description } </p>
+                        </div>
+                    </div>
+
+                    
+                    
+                </div>
+                
+            </Fragment>
         );
     }
 }
--- src/pages/Search/index.jsx
+++ src/pages/Search/index.jsx
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 class Search extends Component {
+    constructor(){
+        super()
+
+        this.state = {
+            results: []
+        };
+
+        this.onSearch = this.onSearch.bind(this);
+    }
+
+    onSearch(event){
+        const value = event.currentTarget.value;
+
+        if(value.length > 3){
+            axios.get(`https://api.mercadolibre.com/sites/MLB/search?q=${value}`)
+             .then(( { data } ) => {
+                this.setState({
+                    results: data.results
+                });
+             });
+        }                                                                                                                       
+        
+    }
+
+    renderItem(item){
+        return (
+            <li key={ item.id }>
+                <span>{ item.id }</span>
+                <span>{ item.title }</span>
+                <Link to={ `/product/${item.id}` }> Abrir produto </Link>
+            </li>
+        )
+    }
+
     render(){
         return (
-            <div>Search</div>
+            <div>
+                <input type="text" onChange={ this.onSearch } />
+
+                <ul>
+                    { this.state.results.map(this.renderItem) }
+                </ul>
+            </div>
         );
     }
 }
