a19c4e66636b6b9cd3a195cf5600ba242c847aa9
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-17 21:59:56 -0300

    Updates

---
+++ precache-manifest.c8a090a6926c9aef2483fc25f53d5bcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ade6e2f0fd89b91759d9b10f8ffa65",
+    "url": "/fiap-react/index.html"
+  },
+  {
+    "revision": "12019a4cbe593639037b",
+    "url": "/fiap-react/static/css/main.21e7a4fa.chunk.css"
+  },
+  {
+    "revision": "83d61ee7b7feaf4b1706",
+    "url": "/fiap-react/static/js/2.b36a1a29.chunk.js"
+  },
+  {
+    "revision": "12019a4cbe593639037b",
+    "url": "/fiap-react/static/js/main.3171dc10.chunk.js"
+  },
+  {
+    "revision": "76f3dd4e9f4f5da98ae8",
+    "url": "/fiap-react/static/js/runtime~main.4f4220b6.js"
+  }
+]);
