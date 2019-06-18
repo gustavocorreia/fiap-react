7118ae40b0959f8cb497cc2af23a30c84d8f17e1
Logon Aluno <logonrmlocal@fiap.com.br>
2019-06-17 22:03:33 -0300

    corrigindo rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -6,6 +6,8 @@ import Product from './pages/Product';
 
 const Routes = () => (
     <Switch>
+        <Route exact path='/fiap-react/' component={ Search } />
+        <Route exact path='/fiap-react/product/:id' component={ Product } />
         <Route exact path='/' component={ Search } />
         <Route exact path='/product/:id' component={ Product } />
         <Route component={ () => (
